c94223550530b59ad5f3d59ee35c5ebc66a32808
jamesmbaazam <james.m.azam@gmail.com>
2025-06-06 15:42:14 +0100

    Rename ratchet plotting script

--- R/fig_panel_diagnostics.R
+++ R/fig_panel_diagnostics.R
@@ -3,17 +3,22 @@ library(ggplot2)
 library(patchwork)
 
 .args <- if (interactive()) {
-    .prov <- "WC"
+    .prov <- "GP"
     sprintf(
         c(
-            file.path("local", "data", c("daily_%s.rds", "weekly_%s.rds")), # cases
+            file.path("local", "data",
+                      c("daily_%s.rds",
+                        "weekly_%s.rds"
+                      )
+            ), # cases
             file.path("local", "output",
                       c("forecast_daily_%s.rds",
                         "forecast_weekly_%s.rds",
-                        "forecast_rescale_%s.rds"
+                        "forecast_rescale_%s.rds",
+                        "diagnostics_%s.csv"
                       )
             ), # forecasts (also contains timing)
-            file.path("local", "figures", "fig_panel_ratchets_%s.png")
+            file.path("local", "figures", "fig_panel_diagnostics_%s.png")
         ), # ratchets
         .prov
     )} else commandArgs(trailingOnly = TRUE)
@@ -27,9 +32,7 @@ weekly_cases <- readRDS(.args[2])
 read_bulk_and_rbind <- function(files, out_type) {
     # Extract the forecast target labels
     forecast_targets <- gsub("^([^_]+)_([^_]+)_([^.]+)\\.rds$", "\\2", files)
-    # Replace "special" with "rescale"; old name -> new name
-    forecast_targets <- ifelse(forecast_targets == "special", "rescale", forecast_targets)
-    setNames(files, forecast_targets) |> # Must always make sure the inputs are in that order 
+    setNames(files, forecast_targets) |> # Must always make sure the inputs are in that order
         lapply(readRDS) |>
         lapply(\(obj) rbindlist(obj[[out_type]])) |>
         rbindlist(idcol = "type", fill = TRUE)
@@ -49,6 +52,9 @@ rachets_dt <- rachets_dt[
     slide_dates_dictionary,
     on = "slide"
 ]
+
+# Diagnostics
+diagnostics_dt <- fread(.args[6])
 
 #####
 #Plots
@@ -89,10 +95,67 @@ ratchets_plot <- ggplot(data = daily_cases) +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
     labs(x = "Date", y = "rachets")
 
-panel_fig <- (cases_plt/ratchets_plot) &
+# Add the dates by slide
+diagnostics_dt <- diagnostics_dt[
+    slide_dates_dictionary,
+    on = "slide"
+]
+
+# Reshape the ESS per sec columns into a single column for plotting
+diagnostics_dt_long <- melt(
+    diagnostics_dt,
+    measure.vars = c("ess_basic", "ess_bulk", "ess_tail"),
+    variable.name = "ess_type",
+    value.name = "ess_value"
+)
+
+# Calculate ESS per sec
+diagnostics_dt_long[, ess_per_sec := ess_value/stan_elapsed_time]
+
+# Shorten ess_type values
+# diagnostics_dt_long[, ess_type := gsub("ess_(.*)", "\\1", ess_type)]
+
+# Plot
+diagnostics_plt <-
+    # First make a layer with all dates present
+    ggplot(data = daily_cases) +
+    geom_blank(
+        aes(x = date, y = max(confirm))
+    ) +
+    # Now add the diagnostics data
+    geom_line(
+        data = diagnostics_dt_long[ess_type == "ess_tail"], # Only plotting ESS tail
+        aes(x = date,
+            y = ess_per_sec,
+            color = type
+        )
+    ) +
+    geom_point(
+        data = diagnostics_dt_long[ess_type == "ess_tail"], # Only plotting ESS tail
+        aes(x = date,
+            y = ess_per_sec,
+            color = type
+        ),
+        size = 1
+    ) +
+    scale_y_log10() +
+    scale_x_date(NULL, date_breaks = "month", date_labels = "%b '%y") +
+    scale_color_brewer(na.translate = FALSE, palette = "Dark2") +
+    labs(
+        # title = "Effective sample size per second",
+        x = "Date",
+        y = "ESS tail per sec (log10)",
+        color = "Forecast target",
+        linetype = "Data"
+    ) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+diagnostics_plt
+
+panel_fig <- (cases_plt/diagnostics_plt/ratchets_plot) &
     plot_layout(ncol = 1, guides = "collect", axes = "collect_x") &
     plot_annotation(title = paste(daily_cases$province[1])) &
     theme_minimal() &
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) 
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 ggsave(tail(.args, 1), panel_fig, bg = "white", width = 12, height = 6)
